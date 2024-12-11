b0f6d8cc9379d5b54cb2517399dfc5a03af113b6
arjun.menon.tblr <arjun.menon.tblr@gmail.com>
2024-12-11 19:10:07 +0530

    added a sign in page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import socketIO from 'socket.io-client';
+import Home from './components/Home';
+import ChatPage from './components/ChatPage';
 const socket = socketIO.connect('http://localhost:4000')
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello World</p>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Home socket={socket} />}></Route>
+          <Route path='/chat' element={<ChatPage socket={socket} />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -12,7 +12,20 @@ const Home = () => {
   };
   return (
     <form className='home_container' onSubmit={handleSubmit}>
-
+      <h2 className='home_header'>Sign In to open chat</h2>
+      <label htmlFor='username'>Username</label>
+      <input
+        type='text'
+        minLength={6}
+        name='username'
+        id='username'
+        className='username_input'
+        value={userName}
+        onChange={(e) => { setUserName(e.target.value) }}
+      />
+      <button className='home_cta'>SIGN_IN</button>
     </form>
   )
 }
+
+export default Home
